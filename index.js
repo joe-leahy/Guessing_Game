4324a308696a90230b1213833cfaaa7d8d16b2d9
Hayden <Haydenchalin@gmail.com>
2022-10-13 22:00:57 -0700

    DONE!! . Just have to style now..

--- index.js
+++ index.js
@@ -1,47 +1,53 @@
-// //game loop
-// function playGame(){
-//     //variables
-//     let randomNumber = Math.floor(Math.random() * 100) + 1;
-//     let playerGuess ;
-
-
-// //    //winning logic
-//    if (playerGuess === randomNumber) {
-//     console.log("you got it")
-//    }
-
-// }
-
 // gets a random number
-function randomNum() { 
-   let number = Math.floor(Math.random() * 100) + 1;
-    console.log(`Random number: ${number}`);
-    return number;
+function randomNum() {
+  let number = Math.floor(Math.random() * 100) + 1;
+  console.log(`Random number: ${number}`);
+  return number;
 }
 
 //for testing,  display random num on screen
-let results = document.getElementById('results')
+let results = document.getElementById("results");
 let random = randomNum();
 
-results.textContent = random;
+// results.textContent = random;
 
-const guessForm = document.getElementById('guess');
-const guessList = document.querySelector('#guesses')
+const guessForm = document.getElementById("guess");
+const guessList = document.querySelector("#guesses");
 
-
+let guessCount = 0;
 
 let guessAttempt;
-document.querySelector('form.guessForm').addEventListener('submit', function (e) {
-    e.preventDefault();  
-    guessAttempt = document.createElement('li')
+document
+  .querySelector("form.guessForm")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    guessAttempt = document.createElement("li");
     guessAttempt.textContent = guessForm.value;
-    guessList.append(guessAttempt);  
-    if (guessForm.value == random) {
+    guessList.append(guessAttempt);
 
-        console.log('You win');
-
+    // this could look a lot cleaner with a switch statement
+    if (guessForm.value == random && !(guessCount > 4)) {
+      console.log(`Nice! You got it on ${guessCount} guesses!`);
+      gameOver();
+    } else if (guessForm.value < random && !(guessCount > 4)) {
+      guessCount++;
+      alert(`You're too low. Guess count: ${guessCount}`);
+      gameOver();
+    } else if (guessForm.value > random && !(guessCount > 4)) {
+      guessCount++;
+      alert(`You're too high. Guess count: ${guessCount}`);
+      gameOver();
+    } else {
+      gameOver();
     }
-}); 
+  
+  });
 
+function gameOver() {
+  if (guessCount > 4) {
+    alert(`Game over you lose!`);
+    window.location.reload();
+  }
+}
 
 // playGame()
